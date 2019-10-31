a51a0645f8e0b423c3e779f973900dad2ab3baab
Bentley <p2c2kbf@gmail.com>
2019-10-31 13:41:47 +0900

    Second Study commit : add tablayout event

--- app/src/main/java/com/share/comin/LectureActivity.kt
+++ app/src/main/java/com/share/comin/LectureActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
 import com.share.comin.Adapter.FragmentAdapter
 import kotlinx.android.synthetic.main.activity_lecture.*
 import kotlinx.android.synthetic.main.tablayout_item.view.*
@@ -23,8 +24,23 @@ class LectureActivity : AppCompatActivity(R.layout.activity_lecture) {
         tablayout.addTab(tablayout.newTab().setCustomView(creatTabView("JS")))
         tablayout.addTab(tablayout.newTab().setCustomView(creatTabView("MP4")))
 
+        list_viewpager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tablayout))
 
-//        tablayout.setupWithViewPager(list_viewpager)
+        tablayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                list_viewpager.currentItem = tab!!.position
+            }
+
+        })
+
     }
 
     private fun creatTabView(tabName: String) : View {
